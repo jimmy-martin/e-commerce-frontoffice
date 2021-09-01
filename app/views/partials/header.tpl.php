6a385dc9b86fd33fec286838a0d60299c372c594
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-01 14:48:55 +0200

    chemins vers images et scripts modifiees

--- app/views/partials/header.tpl.php
+++ app/views/partials/header.tpl.php
@@ -4,9 +4,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
   <title>oShop</title>
 </head>
 
