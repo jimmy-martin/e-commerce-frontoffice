5721dca2103f2af17e2644f21fb992e6092754b3
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-01 14:20:56 +0200

    ajout maincontroller

---
+++ app/controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+class MainController
+{
+    public function home()
+    {
+        $this->show('home');
+    }
+
+    public function show($viewName, $viewVars = [])
+    {
+        require_once __DIR__ . '/../views/partials/header.tpl.php';
+        require_once __DIR__ . '/../views/' . $viewName . '.tpl.php';
+        require_once __DIR__ . '/../views/partials/footer.tpl.php';
+    }
+}
