d2c2e37299862bb88d58877738839fd705aa6248
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-02 12:03:41 +0200

    initialisation du router et creation route home

--- public/index.php
+++ public/index.php
@@ -10,37 +10,56 @@ require_once __DIR__ . '/../app/controllers/MainController.php';
 // ROUTES
 //=============================================================
 
-// On va stocker nos routes dans une variable
-$routes = [
-    '/' => [
-        'controller' => 'MainController',
-        'method' => 'home',
-    ],
-    '/category' => [
-        'controller' => 'MainController',
-        'method' => 'category',
-    ],
-];
+// // On va stocker nos routes dans une variable
+// $routes = [
+//     '/' => [
+//         'controller' => 'MainController',
+//         'method' => 'home',
+//     ],
+//     '/category' => [
+//         'controller' => 'MainController',
+//         'method' => 'category',
+//     ],
+// ];
 
 //=============================================================
 // ROUTER
 //=============================================================
 
-$requestedPageURL = $_GET['_url'] ?? '/';
+//On instancie la classe AltoRouter
+$router = new AltoRouter();
+// dump($router); // Nouvelle daçon de dump avec vardumper
+
+// On fournit a AltoRouter la partie de l'URL à ne pas prendre 
+// en compte pour faire la comparaison entre l'URL courante et 
+// l'url de la route.
+// La valeur de $_SERVER['BASE_URI'] est donnée par le .htaccess. 
+// Elle correspond à la racine du site => à la route "/"
+$router->setBasePath($_SERVER['BASE_URI']);
+// dump($_SERVER);
+
+$router->map(
+    'GET',  //! 1: Methode http (plupart du temps get)
+    '/', //! 2: url de la route (sans la partie fixe)
+    ['controller' => 'MainController', 'method' => 'home',], //! 3: tableau qui stocke le nom du controller et la methode necessaire au fonctionnement du dispatcher
+    'main-home' //! 4 (optionnel): Nom unique de la route
+);
+
+// $requestedPageURL = $_GET['_url'] ?? '/';
 
 // Gestion des mauvaises url 
-if (!array_key_exists($requestedPageURL, $routes)){
-    // pour afficher une erreur 404 je procede comme ci dessous
-    http_response_code(404);
-    exit();
-}
+// if (!array_key_exists($requestedPageURL, $routes)) {
+//     // pour afficher une erreur 404 je procede comme ci dessous
+//     http_response_code(404);
+//     exit();
+// }
 
 // Je peux maintenant acceder a la route correspondante dans le tableau $routes
-$routeInfo = $routes[$requestedPageURL];
+// $routeInfo = $routes[$requestedPageURL];
 
-// On a donc accèes aux infos necessaires
-$controllerName = $routeInfo['controller'];
-$methodName = $routeInfo['method'];
+// // On a donc accèes aux infos necessaires
+// $controllerName = $routeInfo['controller'];
+// $methodName = $routeInfo['method'];
 
 //=============================================================
 // DISPATCHER
@@ -50,4 +69,3 @@ $methodName = $routeInfo['method'];
 $controller = new $controllerName();
 // Et appeler la méthode là aussi dynamiquement
 $controller->$methodName();
-
