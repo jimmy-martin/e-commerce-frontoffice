9a284d8a981349773d131377c49feea6aec3526d
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-02 17:51:59 +0200

    modification view legal

--- app/views/legal.tpl.php
+++ app/views/legal.tpl.php
@@ -3,11 +3,11 @@
       <!-- Breadcrumbs -->
       <ol class="breadcrumb justify-content-center">
         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-        <li class="breadcrumb-item active">Détente</li>
+        <li class="breadcrumb-item active">Mentions légales</li>
       </ol>
       <!-- Hero Content-->
       <div class="hero-content pb-5 text-center">
-        <h1 class="hero-heading">Détente</h1>
+        <h1 class="hero-heading">Mentions légales</h1>
         <div class="row">
           <div class="col-xl-8 offset-xl-2">
             <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
@@ -16,4 +16,9 @@
         </div>
       </div>
     </div>
+  </section>
+  <section class="product-grid">
+      <div class="container-fluid">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi fugit quas mollitia fuga autem necessitatibus, velit, dolores asperiores ducimus dolor non soluta ut corrupti optio eaque illum similique? Reprehenderit pariatur, velit modi facere placeat laboriosam commodi! Quidem similique sit nulla voluptate. Eos architecto saepe temporibus velit, repudiandae quod doloremque dolor exercitationem nam praesentium consequuntur molestias necessitatibus! Id veniam maiores delectus alias, repellat totam aperiam eum eveniet quas mollitia obcaecati sint officiis quo qui dignissimos facere optio voluptate odit! Libero ex nulla, delectus praesentium dolore pariatur quibusdam esse sunt, autem, obcaecati corrupti numquam ullam sint. Tenetur, ipsum? Sequi nulla illum minima, alias iusto fugiat porro error veniam ipsam est atque optio totam exercitationem eaque, fugit excepturi numquam dolorum perspiciatis reprehenderit laborum enim dolorem earum dolor suscipit? A non culpa neque voluptatibus, nemo molestias laborum, dolorem magnam doloribus rerum eligendi? Quo odit laboriosam dolorum quae, dignissimos reprehenderit odio accusamus, adipisci voluptas corporis exercitationem ducimus quibusdam veritatis, enim maiores. Illum error aspernatur quo tenetur impedit delectus repellendus nisi suscipit cupiditate quos id doloremque labore, reiciendis quae distinctio quia autem placeat molestiae beatae nesciunt itaque ad totam voluptates earum. Eveniet aspernatur, beatae non facere fuga sed tempore tenetur porro! Veniam vitae beatae animi ducimus.</p>
+      </div>
   </section>
