53db906dd0305013338ea5db45234d6b7b279a29
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-03 16:13:42 +0200

    product model

--- app/models/Product.php
+++ app/models/Product.php
@@ -1,2 +1,75 @@
 <?php
 
+class Product
+{
+    // =============================================================
+    // PROPERTIES
+    // =============================================================
+
+    private $id;
+    private $name;
+    private $description;
+    private $picture;
+    private $price;
+    private $rate;
+    private $status;
+    private $created_at;
+    private $updated_at;
+    private $brand_id;
+    private $category_id;
+    private $type_id;
+
+    // =============================================================
+    // METHODS
+    // =============================================================
+
+    /**
+     * Return a product by id
+     *
+     * @param [int] $id ID of the searched product in database
+     * @return [object] $productObject
+     */
+    public function find($id)
+    {        
+        $pdo = Database::getPDO();
+
+        $sql = 'SELECT * FROM `product` WHERE `id` = ' . $id;
+
+        $statement = $pdo->query($sql);
+
+        $productObject = $statement->fetchObject('Product');
+
+        return $productObject;
+    }
+
+    /**
+     * Return all the brands
+     *
+     * @return [object] $allProductObjects
+     */
+    public function findAll()
+    {
+        $pdo = Database::getPDO();
+
+        $sql = 'SELECT * FROM `product`';
+
+        $statement = $pdo->query($sql);
+
+        $allProductObjects = $statement->fetchAll(PDO::FETCH_CLASS, 'Product');
+
+        return $allProductObjects;
+    }
+
+    // =============================================================
+    // GETTERS & SETTERS
+    // =============================================================
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
