4711460ceaf174960b8f2231794aabc900d2d14d
jimmy-martin <jimmy.martin952@gmail.com>
2021-09-08 09:10:03 +0200

    liens product.list ok

--- app/views/product.list.tpl.php
+++ app/views/product.list.tpl.php
@@ -42,18 +42,18 @@
         <?php foreach($products as $product) :?>
         <div class="product col-xl-3 col-lg-4 col-sm-6">
           <div class="product-image">
-            <a href="detail.html" class="product-hover-overlay-link">
+            <a href="<?= $router->generate('catalog-product', ['id' => $product->getId()]) ?>" class="product-hover-overlay-link">
               <img src="<?= $absoluteURL ?>/<?= $product->getPicture() ?>" alt="product" class="img-fluid">
             </a>
           </div>
           <div class="product-action-buttons">
             <a href="#" class="btn btn-outline-dark btn-product-left"><i class="fa fa-shopping-cart"></i></a>
-            <a href="detail.html" class="btn btn-dark btn-buy"><i class="fa-search fa"></i><span
+            <a href="<?= $router->generate('catalog-product', ['id' => $product->getId()]) ?>" class="btn btn-dark btn-buy"><i class="fa-search fa"></i><span
                 class="btn-buy-label ml-2">Voir</span></a>
           </div>
           <div class="py-2">
             <p class="text-muted text-sm mb-1"><?= $type->find($product->getTypeID())->getName() ?></p>
-            <h3 class="h6 text-uppercase mb-1"><a href="detail.html" class="text-dark"><?= $product->getName() ?></a></h3><span
+            <h3 class="h6 text-uppercase mb-1"><a href="<?= $router->generate('catalog-product', ['id' => $product->getId()]) ?>" class="text-dark"><?= $product->getName() ?></a></h3><span
               class="text-muted"><?= $product->getPrice() ?>€</span>
           </div>
         </div>
